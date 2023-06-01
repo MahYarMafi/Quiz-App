4f60677048851c1f2390de9fb43f4d1a41cfa353
MahYar <mahyar.mafi@gmail.com>
2023-05-31 23:36:31 -0400

    new Constants added, ResultActivity UI items functionality added, btnFinish coded. Intents and putExtra methods added.

--- app/src/main/java/com/mahyar/quizapp/QuizQuestionsActivity.kt
+++ app/src/main/java/com/mahyar/quizapp/QuizQuestionsActivity.kt
@@ -1,5 +1,6 @@
 package com.mahyar.quizapp
 
+import android.content.Intent
 import android.graphics.Color
 import android.graphics.Typeface
 import androidx.appcompat.app.AppCompatActivity
@@ -13,6 +14,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     private var mCurrentPosition: Int = 1
     private var mQuestionList: ArrayList<Question>? = null
     private var mSelectedOptionPosition: Int = 0
+    private var mUserName: String? = null
+    private var mCorrectAnswers: Int = 0
 
     private var progressBar: ProgressBar? = null
     private var txtProgress: TextView? = null
@@ -27,6 +30,8 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_quiz_questions)
+
+        mUserName = intent.getStringExtra(Constants.USER_NAME)
 
         progressBar = findViewById(R.id.progressBar)
         txtProgress = findViewById(R.id.txtProgress)
@@ -135,15 +140,23 @@ class QuizQuestionsActivity : AppCompatActivity(), View.OnClickListener {
                         mCurrentPosition <= mQuestionList!!.size -> {
                             setQuestion()
                         }
-                        else ->{
-                            Toast.makeText(this,"You made it to the end.",Toast.LENGTH_LONG).show()
+                        else -> {
+                            val intent = Intent(this, ResultActivity::class.java)
+                            intent.putExtra(Constants.USER_NAME, mUserName)
+                            intent.putExtra(Constants.CORRECT_ANSWERS, mCorrectAnswers)
+                            intent.putExtra(Constants.TOTAL_QUESTIONS, mQuestionList?.size)
+                            startActivity(intent)
+                            finish()
                         }
+
                     }
 
                 } else {
                     val question = mQuestionList?.get(mCurrentPosition - 1)
                     if (question!!.correctAnswer != mSelectedOptionPosition) {
                         answerView(mSelectedOptionPosition, R.drawable.wrong_option_border_bg)
+                    } else {
+                        mCorrectAnswers++
                     }
                     answerView(question.correctAnswer, R.drawable.correct_option_border_bg)
 
--- app/src/main/java/com/mahyar/quizapp/ResultActivity.kt
+++ app/src/main/java/com/mahyar/quizapp/ResultActivity.kt
@@ -1,11 +1,31 @@
 package com.mahyar.quizapp
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+import android.widget.TextView
+import org.w3c.dom.Text
 
 class ResultActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_result)
+
+        val tvNames: TextView = findViewById(R.id.tvNames)
+        val tvScore: TextView = findViewById(R.id.tvScore)
+        val btnFinish: Button = findViewById(R.id.btnFinish)
+
+        tvNames.text = intent.getStringExtra(Constants.USER_NAME)
+
+        val totalQuestions = intent.getIntExtra(Constants.TOTAL_QUESTIONS, 0)
+        val correctAnswers = intent.getIntExtra(Constants.CORRECT_ANSWERS, 0)
+
+        tvScore.text = "Your Score is $correctAnswers out of $totalQuestions"
+
+        btnFinish.setOnClickListener {
+            startActivity(Intent(this, MainActivity::class.java))
+        }
+
     }
 }
